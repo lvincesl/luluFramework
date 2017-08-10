4d97cf2fb5d01e42e7c61b13b81500db0520db6f
Lionel Vinceslas <lionel.vinceslas@eurower.net>
2017-08-10 17:33:01 -0400

    Added C3.js support

---
+++ src/Client/View/C3.php
@@ -0,0 +1,8 @@
+<?php
+namespace Luluframework\Client\View;
+
+Interface C3Interface
+{
+    public function __construct();
+    public function js();
+}
---
+++ src/Client/View/C3/Donut.php
@@ -0,0 +1,20 @@
+<?php
+namespace Luluframework\Client\View\C3;
+
+class Donut implements \Luluframework\Client\View\C3
+{
+    public function __construct()
+    {
+        
+    }
+
+    /**
+     * Return the Donut javascript source code
+     *
+     * @return string
+     */
+    public function js()
+    {
+
+    }
+}
