56bd6be513cc178958eedc044e247962f619b143
Lionel Vinceslas <lionel.vinceslas@eurower.net>
2017-08-21 14:36:28 -0400

    Added Button support

---
+++ src/Client/View/Button.php
@@ -0,0 +1,261 @@
+<?php
+
+namespace Luluframework\Client\View;
+
+class Button
+{
+    const DEFAULT   = 0;
+    const PRIMARY   = 1;
+    const SUCCESS   = 2;
+    const INFO      = 3;
+    const WARNING   = 4;
+    const DANGER    = 5;
+    const LINK      = 6;
+
+    private $text;
+    private $tooltip;
+    private $class;
+    private $type;
+    private $modalId;
+    private $onclick;
+
+    public function __construct($text=null)
+    {
+        if (!is_string($text) && !is_null($text)) {
+            throw new \Exception("InvalidArgumentException : string expected for 'text' !");
+        } else {
+            $this->text     = $text;
+            $this->tooltip  = null;
+            $this->class    = null;
+            $this->type     = Button::DEFAULT;
+            $this->modalId  = null;
+            $this->onclick  = null;
+        }
+    }
+
+    /**
+     * Set button text
+     *
+     * @param string $id
+     * @return boolean
+     */
+    public function setText($text)
+    {
+        if (!is_string($text))
+        {
+            return false;
+        } else {
+            $this->text = $text;
+            return true;
+        }
+    }
+
+    /**
+     * Get button text
+     *
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * Set button tooltip
+     *
+     * @param string $id
+     * @return boolean
+     */
+    public function setTooltip($tooltip)
+    {
+        if (!is_string($tooltip))
+        {
+            return false;
+        } else {
+            $this->tooltip = $tooltip;
+            return true;
+        }
+    }
+
+    /**
+     * Get button tooltip
+     *
+     * @return string
+     */
+    public function getTooltip()
+    {
+        return $this->tooltip;
+    }
+
+    /**
+	 * Set button class
+	 *
+	 * @param string $class
+	 * @return boolean
+	 */
+	public function setClass($class) 
+	{
+		if (!is_string($class)) {
+			return false;
+		} else {
+			$this->class = $class;
+			return true;
+		}
+	}
+
+	/**
+	 * Get button class
+	 *
+	 * @return string
+	 */
+	public function getClass()
+	{
+		return $this->class;
+    }
+    
+    /**
+     * Set button type
+     *
+     * @param integer $type
+     * @return boolean
+     */
+    public function setType($type)
+    {
+        if (!is_integer($type)) {
+            return false;
+        } elseif (  $type != Button::DEFAULT &&
+                    $type != Button::INFO &&
+                    $type != Button::PRIMARY &&
+                    $type != Button::SUCCESS &&
+                    $type != Button::WARNING &&
+                    $type != Button::DANGER &&
+                    $type != Button::LINK) {
+            return false;
+        } else {
+            $this->type = $type;
+            return true;
+        }
+    }
+
+    /**
+     * Get button type
+     *
+     * @return integer
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+	 * Set modal id
+	 *
+	 * @param string $class
+	 * @return boolean
+	 */
+	public function setModalId($modalId) 
+	{
+		if (!is_string($modalId)) {
+			return false;
+		} else {
+			$this->modalId = $modalId;
+			return true;
+		}
+	}
+
+	/**
+	 * Get modal id
+	 *
+	 * @return string
+	 */
+	public function getModalId()
+	{
+		return $this->modalId;
+    }
+
+    /**
+	 * Set onclick property
+	 *
+	 * @param string $class
+	 * @return boolean
+	 */
+	public function setOnclick($onclick) 
+	{
+		if (!is_string($onclick)) {
+			return false;
+		} else {
+			$this->onclick = $onclick;
+			return true;
+		}
+	}
+
+	/**
+	 * Get onclick property
+	 *
+	 * @return string
+	 */
+	public function getOnclick()
+	{
+		return $this->onclick;
+    }
+
+    /**
+     * Return the button html source code
+     *
+     * @return string
+     */
+    public function html()
+    {
+        switch ($this->type) {
+            case Panel::DEFAULT:
+                $this->class .= " btn-default";
+                break;
+
+            case Panel::PRIMARY:
+                    $this->class .= " btn-primary";
+                    break;
+
+            case Panel::INFO:
+                    $this->class .= " btn-info";
+                    break;
+
+            case Panel::SUCCESS:
+                    $this->class .= " btn-success";
+                    break;
+
+            case Panel::WARNING:
+                    $this->class .= " btn-warning";
+                    break;
+
+            case Panel::DANGER:
+                    $this->class .= " btn-danger";
+                    break;
+
+            case Panel::LINK:
+                    $this->class .= " btn-link";
+                    break;
+            
+            default:
+                $this->class .= " btn-default";
+                break;
+        }
+
+        $html = "<button type='button' class='btn {$this->class}'";
+        
+        if (strlen($this->tooltip)) {
+            $html .= " data-toggle='tooltip' title=\"{$this->tooltip}\"";
+        } 
+        
+        if (strlen($this->modalId)) {
+            $html .= " data-toggle='modal' data-target='#{$this->modalId}'";
+        }
+
+        if (strlen($this->onclick)) {
+            $html .= " onclick=\"#{$this->onclick}\"";
+        }
+
+        $html .= ">{$this->text}</button>";
+        
+        return $html;
+    }
+}
---
+++ tests/ButtonTest.php
@@ -0,0 +1,192 @@
+<?php
+
+use \Luluframework\Client\View\Button;
+
+class ButtonTest extends \PHPUnit\Framework\TestCase
+{
+
+    public function test__constructWithGoodValue()
+    {
+        $this->assertInstanceOf(Button::class, new Button('ok'));
+    }
+
+    public function test__constructWithBadValue()
+    {
+        $this->expectException(Exception::class);
+        $b =  new Button(7);
+    }
+
+    public function testSetTextWithoutValue()
+    {
+        $this->expectException(\PHPUnit\Framework\Error\Error::class);
+        $b = new Button();
+        $b->setText();
+    }
+
+    public function testSetTextWithGoodValue()
+    {
+        $b = new Button();
+        $this->assertEquals(true, $b->setText('Ok'));
+    }
+
+    public function testSetTextWithBadValue()
+    {
+        $b = new Button();
+        $this->assertEquals(false, $b->setText(12));
+    }
+
+    public function testGetText()
+    {
+        $b = new Button();
+        $this->assertEquals(null, $b->getText());
+
+        $b->setText('Ok');
+        $this->assertInternalType('string', $b->getText());
+    }
+
+    public function testSetTooltipWithoutValue()
+    {
+        $this->expectException(\PHPUnit\Framework\Error\Error::class);
+        $b = new Button();
+        $b->setTooltip();
+    }
+
+    public function testSetTooltipWithGoodValue()
+    {
+        $b = new Button();
+        $this->assertEquals(true, $b->setTooltip('Ok'));
+    }
+
+    public function testSetTooltipWithBadValue()
+    {
+        $b = new Button();
+        $this->assertEquals(false, $b->setTooltip(12));
+    }
+
+    public function testGetTooltip()
+    {
+        $b = new Button();
+        $this->assertEquals(null, $b->getTooltip());
+
+        $b->setTooltip('Ok');
+        $this->assertInternalType('string', $b->getTooltip());
+    }
+
+    public function testSetClassWithoutValue()
+    {
+        $this->expectException(\PHPUnit\Framework\Error\Error::class);
+        $b = new Button();
+        $b->setClass();
+    }
+
+    public function testSetClassWithGoodValue()
+    {
+        $b = new Button();
+        $this->assertEquals(true, $b->setClass('active'));
+    }
+
+    public function testSetClassWithBadValue()
+    {
+        $b = new Button();
+        $this->assertEquals(false, $b->setClass(12));
+    }
+
+    public function testSetTypeWithoutValue()
+    {
+        $this->expectException(\PHPUnit\Framework\Error\Error::class);
+        $b = new Button();
+        $b->setType();
+    }
+
+    public function testSetTypeWithGoodValue()
+    {
+        $b = new Button();
+        $this->assertEquals(true, $b->setType(Button::SUCCESS));
+    }
+
+    public function testSetTypeWithBadValue()
+    {
+        $b = new Button();
+        $this->assertEquals(false, $b->setType('7'));
+    }
+
+    public function testGetType()
+    {
+        $b = new Button();
+        $this->assertEquals(null, $b->getType());
+
+        $b->setType(Button::SUCCESS);
+        $this->assertInternalType('integer', $b->getType());
+    }
+
+    public function testGetClass()
+    {
+        $b = new Button();
+        $this->assertEquals(null, $b->getClass());
+
+        $b->setClass('active');
+        $this->assertInternalType('string', $b->getClass());
+    }
+
+    public function testSetModalIdWithoutValue()
+    {
+        $this->expectException(\PHPUnit\Framework\Error\Error::class);
+        $b = new Button();
+        $b->setModalId();
+    }
+
+    public function testSetModalIdWithGoodValue()
+    {
+        $b = new Button();
+        $this->assertEquals(true, $b->setModalId("test"));
+    }
+
+    public function testSetModalIdWithBadValue()
+    {
+        $b = new Button();
+        $this->assertEquals(false, $b->setModalId(7));
+    }
+
+    public function testGetModalId()
+    {
+        $b = new Button();
+        $this->assertEquals(null, $b->getModalId());
+
+        $b->setModalId("test");
+        $this->assertInternalType('string', $b->getModalId());
+    }
+
+    public function testSetOnclickWithoutValue()
+    {
+        $this->expectException(\PHPUnit\Framework\Error\Error::class);
+        $b = new Button();
+        $b->setModalId();
+    }
+
+    public function testSetOnclickWithGoodValue()
+    {
+        $b = new Button();
+        $this->assertEquals(true, $b->setOnclick("test"));
+    }
+
+    public function testSetOnclickWithBadValue()
+    {
+        $b = new Button();
+        $this->assertEquals(false, $b->setOnclick(7));
+    }
+
+    public function testGetOnclick()
+    {
+        $b = new Button();
+        $this->assertEquals(null, $b->getOnclick());
+
+        $b->setOnclick("test");
+        $this->assertInternalType('string', $b->getOnclick());
+    }
+
+    public function testHtml()
+    {
+        $b = new Button();
+        $this->assertInternalType('string', $b->html());
+    }
+}
