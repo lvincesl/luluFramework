0e815af968d18b119f0554e91592f6ef0553397b
Lionel Vinceslas <lionel.vinceslas@groupseen.com>
2018-05-18 15:06:24 -0400

    Fix a minor git issue

--- src/Server/AclManager.php
+++ src/Server/AclManager.php
@@ -6,4 +6,37 @@ namespace Luluframework\Server;
 
 class AclManager extends Zend\Permissions\Acl\Acl {
 
+	/**
+	 * Acl json file url
+	 * @var string
+	 */
+	protected $aclJsonFile;
+
+	/**
+	 * Acl confguration array
+	 * @var array
+	 */
+	protected $aclConfiguration [];
+
+
+	function saveAclJsonFile($aclJsonFile = null)
+	{
+
+	}
+
+	function parseAclJsonFile($aclJsonFile = null)
+	{
+
+	}
+
+	function hasFilter($filter)
+	{
+
+	}
+
+	function getFilters()
+	{
+		
+	}
+
 }
